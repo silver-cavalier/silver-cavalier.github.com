98737e11bcb2097884229f35e63457271a37e5bf
Sun <1459025153@qq.com>
2023-11-18 22:06:41 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,28 +1,93 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# def hello():
+#     return render_template('index.html', name=name, movies=movies)
+
+
+import os
+import sys
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+WIN = sys.platform.startswith('win')
+if WIN: # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else: # 否则使用四个斜线
+    prefix = 'sqlite:////'
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 关闭对模型修改的监控
 
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)
 
-name = 'Buyan Sun'
-movies = [  {'title': 'My Neighbor Totoro', 'year': '1988'},
-            {'title': 'Dead Poets Society', 'year': '1989'},
-            {'title': 'A Perfect World', 'year': '1993'},
-            {'title': 'Leon', 'year': '1994'},
-            {'title': 'Mahjong', 'year': '1996'},
-            {'title': 'Swallowtail Butterfly', 'year': '1996'},
-            {'title': 'King of Comedy', 'year': '1999'},
-            {'title': 'Devils on the Doorstep', 'year': '1999'},
-            {'title': 'WALL-E', 'year': '2008'},
-            {'title': 'The Pork of Music', 'year': '2012'},  ]
+# 创建数据库模型
 
+class User(db.Model): # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True) # 主键
+    name = db.Column(db.String(20)) # 名字
 
+class Movie(db.Model): # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True) # 主键
+    title = db.Column(db.String(60)) # 电影标题
+    year = db.Column(db.String(4)) # 电影年份
+
+# 编写一个自定义命令来自动执行创建数据库表操作
+
+import click
+@app.cli.command() # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')
+# 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop: # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.') # 输出提示信息
+
+# 在主页视图读取数据库记录
 
 @app.route('/')
-def hello():
-    return render_template('index.html', name=name, movies=movies)
+def index():
+    user = User.query.first() # 读取用户记录
+    movies = Movie.query.all() # 读取所有电影记录
+    return render_template('index.html', user=user, movies=movies)
+
+# 注：在 index 视图中，原来传入模板的 name 变量被 user 实例取代，模板 index.html 中的两处 name 变量也要相应的更新为 user.name 属性
 
 
+# 生成虚拟数据
 
+import click
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
 
+    # 全局的两个变量移动到这个函数内
+    name = 'Buyan Sun'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
 
-
-
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+    
+    db.session.commit()
+    click.echo('Done.')
